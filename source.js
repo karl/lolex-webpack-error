37ced636235879e447431106480afc354a163e6c
Karl O'Keeffe <karl@geckoboard.com>
2016-04-07 17:34:57 +0100

    Show that the lolex.js built by Browserify fails.
    This Browserify bundle still throws an error in the browser because `process` does not exist.

--- source.js
+++ source.js
@@ -1,5 +1,9 @@
 console.log('start');
 
-var lolex = require('lolex');
+// Uncomment this to use latest `lolex-src.js` from `master` branch.
+// var lolex = require('lolex');
+
+// This uses the latest `lolex.js` as transformed by browserify.
+var lolex = require('./lolex.js');
 
 console.log('done');
